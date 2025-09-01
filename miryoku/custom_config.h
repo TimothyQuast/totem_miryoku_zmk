286690b3b2361010a6365c8d7c9c3540471acf7c
TimothyQuast <32230585+TimothyQuast@users.noreply.github.com>
2025-09-01 08:08:54 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,10 +1,2 @@
 // Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
-include:
-  - board: seeeduino_xiao_ble
-    shield: totem_left
-  - board: seeeduino_xiao_ble
-    shield: totem_right
-  - board: seeeduino_xiao_ble
-    shield: settings_reset
+// https://github.com/manna-harbour/mi
